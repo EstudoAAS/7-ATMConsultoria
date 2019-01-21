639a31e4cf3b5ad62755bff95e1d5ea9edb5fbee
Aller Augusto de Souza <aller.souza@samsung.com>
2019-01-21 11:12:38 -0200

    Add information to about page

--- app/src/main/java/com/hudeing/atmconsultoria/activity/SobreActivity.java
+++ app/src/main/java/com/hudeing/atmconsultoria/activity/SobreActivity.java
@@ -13,11 +13,23 @@ public class SobreActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_sobre);
 
+        // setContentView(R.layout.activity_sobre);
+        String descricao = "A ATM Consultoria tem como missão apoiar as organizações que desejam alcançar o sucesso através da excelência em gestão e da busca pela Qualidade.\n\n" +
+                "Nosso trabalho é dar suporte às empresas que desejam se certicar em padrões de qualidade ou investir no desenvolvimento e evolução de sua gestão, por meio da otimização dos processos e da disseminação dos Fundamentos e Critérios de Excelência";
         View sobre = new AboutPage(this)
                 .setImage(R.drawable.logo)
+                .setDescription(descricao)
                 .addGroup("Fale conosco")
+                .addEmail("atmcolsultoria@gmail.com","Envie um e-mail")
+                .addWebsite("http://google.com.br/", "Acesse nosso site")
+                .addGroup("Acesse nossas redes sociais")
+                .addFacebook("google", "Acesse nosso facebook")
+                .addTwitter("google", "Twitter")
+                .addYoutube("google", "Youtube")
+                .addPlayStore("com.google.android.apps.plus", "Play Store")
+                .addGitHub("google", "Github")
+                .addInstagram("google", "Instagram")
                 .create();
 
         setContentView(sobre);
